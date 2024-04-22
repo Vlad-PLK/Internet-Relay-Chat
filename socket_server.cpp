5345b8523262e00c88704710a4e2b5ef14cc3ff9
Vlad-PLK <leonpolo365@gmail.com>
2024-04-22 14:42:08 +0200

    creating socket for listening

--- socket_server.cpp
+++ socket_server.cpp
@@ -6,30 +6,49 @@
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 15:58:44 by vpolojie          #+#    #+#             */
-/*   Updated: 2024/04/19 17:26:17 by vpolojie         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:26:07 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
 
 void    ft_create_socket(int port)
 {
     int sockfd;
-    struct sockaddr_in adr;
+    struct sockaddr_in addr;
+
     //AF_INET = IPv4 Internet Protocol
     //SOCK_STREAM = socket providing sequenced, reliable, two way communications
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
         perror("socket not created !");
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    listen(sockfd, 10);
+    ///accept(sockfd, (struct sockaddr *)&addr, addr);
     //  sockfd = socket(int socket_family, int socket_type, int protocol); //
         //  socket -> creates a socket
+        //  bind -> create the range of addresses to listen from
         //  connect -> connects a socket  to  a  remote socket  address
         //  listen -> tells the socket that new connections shall be accepted
         //  accept -> is used to get a new socket with a new incoming connection
         //  poll and select wait for arriving data or readiness to send data
             // + the standard input output operations like write, send, read can be used to read and 
             // write data
+    /*
+        When a socket is created with socket(2), it exists in a name space (ad‐
+       dress family) but has no address assigned to it.   bind()  assigns  the
+       address  specified  by  addr  to the socket referred to by the file de‐
+       scriptor sockfd.  addrlen specifies the size, in bytes, of the  address
+       structure  pointed to by addr.  Traditionally, this operation is called
+       “assigning a name to a socket”.
+
+    */
     return ;
 }
