4488defc4803dfa161805992faae5abc68665983
Thomas Vincileoni <thomasvinci@monaco.mc>
2024-05-31 15:22:44 +0300

    Merge functions corrections 1

--- commands/mode.cpp
+++ commands/mode.cpp
@@ -1,10 +1,12 @@
-#include "../SocketServer.hpp"
+#include "../Command.hpp"
 
-void    SocketServer::mode(User &user, std::vector<std::string> params)
+void    mode(User &user, Channel &channel_void, SocketServer &server, std::vector<std::string> &params)
 {
+    (void)channel_void;
+    (void)server;
     if (params.size() < 2)
     {
-        user.my_send((ERR_NEEDMOREPARAMS(user.getNickname(), "MODE")).c_str());
+        user.usr_send((ERR_NEEDMOREPARAMS(user.getNickname(), "MODE")).c_str());
         return ;
     }
 
