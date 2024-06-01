cfb1f5d1e9fdad2e749810e64fcfd2ff890dbee5
Vlad-PLK <leonpolo365@gmail.com>
2024-06-01 08:42:38 +0200

    working on double nickname

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -1,21 +1,81 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   nick.cpp                                           :+:      :+:    :+:   */
+/*   Nick.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 10:14:57 by vpolojie          #+#    #+#             */
-/*   Updated: 2024/05/29 12:21:26 by vpolojie         ###   ########.fr       */
+/*   Updated: 2024/06/01 08:33:36 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Command.hpp"
 
-int check_nickname_in_use(const std::string &nick, const std::vector<User> &users)
+int	ft_nbn(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n <= 0)
+		i = 1;
+	while (n != 0)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
+
+void	ft_putnbr(int nb, int *i, char *str)
+{
+	if (nb == -2147483648)
+	{
+		str[0] = '-';
+		(*i)++;
+		ft_putnbr(21474, i, str);
+		ft_putnbr(83648, i, str);
+	}
+	else if (0 <= nb && nb <= 9)
+		str[(*i)++] = nb + '0';
+	else if (nb < 0)
+	{
+		str[0] = '-';
+		(*i)++;
+		ft_putnbr(nb *(-1), i, str);
+	}
+	else
+	{
+		ft_putnbr(nb / 10, i, str);
+		ft_putnbr(nb % 10, i, str);
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (n < 0)
+		str = (char *)malloc((ft_nbn(n) + 2) * sizeof(char));
+	else
+		str = (char *)malloc((ft_nbn(n) + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	if (n == 0)
+		str[0] = '0';
+	ft_putnbr(n, &i, str);
+	str[i] = '\0';
+	return (str);
+}
+
+
+int check_nickname_in_use(std::string &nick, const std::vector<User> &users)
 {
     for (size_t i = 0; i != users.size(); i++)
     {
+        srand(time(0));
         // NEED TO FIX IT WITH SERVER PART //
         if (nick == users[i].getNickname())
             return (1);
@@ -40,7 +100,10 @@ void					nick(User &user, Channel &channel, SocketServer &server, std::vector<st
     (void)channel;
     (void)server;
     if (check_nickname_in_use(params.front(), server.getAllUsers()) == 1)
-        user.usr_send(ERR_NICKNAMEINUSE(params.front()));
+    {
+        user.usr_send("");
+        user.setNickname(params.front() + ft_itoa(rand() % 1000));
+    }
     else if (check_nickname_validity(params.front()) == 1)
         user.usr_send(ERR_ERRONEUSNICKNAME(params.front()));
     else if (params.front().size() == 0)
